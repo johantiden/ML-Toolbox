2df1636d5f94a35547dd1a611037940ad20887cc
jtiden <tidenjohan@gmail.com>
2014-05-20 01:12:03 +0200

    Implemented locked candidates

---
+++ sudoku/src/main/java/se/jtiden/sudoku/domain/Board.java
@@ -0,0 +1,38 @@
+package main.java.se.jtiden.sudoku.domain;
+
+import main.java.se.jtiden.sudoku.solver.SudokuNodeSet;
+import main.java.se.jtiden.sudoku.struct.Coordinate;
+
+import java.util.Collection;
+
+public interface Board {
+    void setHardDigit(Coordinate coordinate, int value);
+
+    boolean isHard(Coordinate coordinate);
+
+    int numCandidatesLeft(Coordinate coordinate);
+
+    void removeCandidate(Coordinate coordinate, int value);
+
+    void solveNode(Coordinate coordinate, int value);
+
+    int getValue(Coordinate coordinate);
+
+    Collection<? extends UnsolvedNode> getUnsolvedNodes();
+
+    Iterable<? extends Coordinate> getAllCoordinates();
+
+    Iterable<? extends Node> getNeighborsFor(Node node);
+
+    int countCandidatesInRowFor(Coordinate coordinate, int value);
+
+    int countCandidatesInColumnFor(Coordinate coordinate, int value);
+
+    int countCandidatesInBoxFor(Coordinate coordinate, int value);
+
+    int getOrder();
+
+    Iterable<? extends SudokuNodeSet> getAllRowSubGroups();
+
+    Iterable<? extends SudokuNodeSet> getAllColumnSubGroups();
+}
